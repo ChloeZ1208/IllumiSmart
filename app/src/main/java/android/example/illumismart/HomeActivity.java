71a7f081cdc08d4e8fb9a03a99b23dd8cb58832f
ChloeZ1208 <dongqingzhu1208@hotmail.com>
2021-02-22 23:40:17 +0800

    home page finished

--- app/src/main/java/android/example/illumismart/HomeActivity.java
+++ app/src/main/java/android/example/illumismart/HomeActivity.java
@@ -4,11 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class HomeActivity extends AppCompatActivity {
+    private BottomNavigationView bottomNav;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        bottomNav = findViewById(R.id.bottom_navigation);
+
+        bottomNav.getMenu().findItem(R.id.home_page).setChecked(true);
+
     }
 }
