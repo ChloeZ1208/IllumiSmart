51f9737ee73797a7750de8787a15feae9eb0bef9
suzhe96 <oliversu96@yahoo.com>
2021-03-02 19:17:15 +0800

    Zhe: add flicker detection functionality

--- app/src/main/java/android/example/illumismart/HomeActivity.java
+++ app/src/main/java/android/example/illumismart/HomeActivity.java
@@ -12,23 +12,31 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.card.MaterialCardView;
 
 public class HomeActivity extends AppCompatActivity {
-    private BottomNavigationView bottomNav;
-    private MaterialCardView enterLightLevel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        bottomNav = findViewById(R.id.bottom_navigation);
-        enterLightLevel = findViewById(R.id.enter_light_level);
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        MaterialCardView enterLightLevel = findViewById(R.id.enter_light_level);
+        MaterialCardView enterLightFlicker = findViewById(R.id.enter_light_flicker);
 
         bottomNav.getMenu().findItem(R.id.home_page).setChecked(true);
 
         enterLightLevel.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(HomeActivity.this, LightLevelActivity.class));
+                startActivity(new Intent(HomeActivity.this,
+                        LightLevelActivity.class));
+            }
+        });
+
+        enterLightFlicker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(HomeActivity.this,
+                        FlickerActivity.class));
             }
         });
 
--- app/src/main/java/android/example/illumismart/LightLevelActivity.java
+++ app/src/main/java/android/example/illumismart/LightLevelActivity.java
@@ -150,22 +150,24 @@ public class LightLevelActivity extends AppCompatActivity {
             public void onClick(View v) {
                 // Initialize tmp list for lux
                 luxMeasurementTmpList.clear();
-                // Flag on: insert lux to the tmp list
-                luxMeasurementActivated = true;
                 // Clear previous average lux display
                 luxMeasurementAverage.setText(" ");
                 // Countdown text display
                 luxMeasurementHeader.setVisibility(View.VISIBLE);
                 // Start timer
                 luxMeasurementTimer.start();
+                // Flag on: insert lux to the tmp list
+                luxMeasurementActivated = true;
             }
         });
 
         lightLevelPause.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                luxMeasurementTimer.cancel();
-                luxMeasurementTimer.onFinish();
+                if (luxMeasurementActivated) {
+                    luxMeasurementTimer.cancel();
+                    luxMeasurementTimer.onFinish();
+                }
             }
         });
 
@@ -187,6 +189,9 @@ public class LightLevelActivity extends AppCompatActivity {
                             (timeStamp, String.valueOf(minLux), String.valueOf(maxLux),
                                     luxMeasurementAverage.getText().toString());
                     illuminanceViewModel.insert(illuminance);
+                    luxMeasurementTmpList.clear();
+                    Toast.makeText(LightLevelActivity.this,
+                            "Data saved!", Toast.LENGTH_SHORT).show();
                 } else {
                     Toast.makeText(LightLevelActivity.this,
                             "No data to save. Click play!", Toast.LENGTH_SHORT).show();
